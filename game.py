90118ded555ae9c7c971032c2b019940bbd0761b
Sharath S <sharaths21312@gmail.com>
2020-11-18 18:26:07 +0530

    Draw Os and Xs

--- game.py
+++ game.py
@@ -12,44 +12,55 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # by changing the working directory to the program directory
 
 # User-adjustable variables
-MAXFPS = 30 # Maximun FPS, to prevent too much CPU usage
+MAXFPS = 30  # Maximun FPS, to prevent too much CPU usage
 theme = "dark"
-
+# Dark vs light colors
+Colorpallette = {
+    "light": {
+        "background": (245, 245, 245),
+        "grid": (0, 0, 0),
+        "symbol": (0, 0, 0)
+    },
+    "dark": {
+        "background": (10, 10, 10),
+        "grid": (255, 255, 255),
+        "symbol": (200, 100, 200)
+    }
+}
 
 # Internal variables
 size = (600, 600)
 lastTime = time.time()
 RUNNING = True
-state = ((False, False, False), (False, False, False), (False, False, False))
+state = (0, 0, 0,
+         0, 0, 0,
+         0, 0, 0)  # 0 -> empty; 1 -> X; -1 -> O
+
+coordinates = ((160, 160), (300, 160), (440, 160),
+               (160, 300), (300, 300), (440, 300),
+               (160, 440), (300, 440), (440, 440))
+
 window = pygame.display.set_mode(size)
-# surface = pygame.Surface(size)
 pygame.display.set_caption("Tic Tac Toe")
 try:
     pygame.display.set_icon(pygame.image.load("Resources\\icon.png"))
 except FileNotFoundError:
     pygame.display.set_icon(pygame.image.load("Resources/icon.png"))
 
+# Functions
 
+def drawcross(color, coords, h: float):
+    pygame.draw.line(window, color, (coords[0] - h, coords[1] - h), (coords[0] + h, coords[0] + h), 4)
+    pygame.draw.line(window, color, (coords[0] - h, coords[1] + h), (coords[1] + h, coords[1] - h), 4)
 
-# Dark vs light colors
-Colorpallette = {
-    "light":{
-        "background":(245,245,245),
-        "grid": (0, 0, 0)
-    },
-    "dark":{
-        "background":(10,10,10),
-        "grid": (255, 255, 255)
-    }
-}
-
-
-# Functions: To be moved to separate file
 def gameloop(event):
     """Main game loop"""
-    if event.type == 256: # Close button pressed
+    if event.type == 256:  # Close button pressed
         pygame.quit()
         exit(0)
+    
+
+
 
 def draw():
     # Draw the grid
@@ -57,8 +68,12 @@ def draw():
     pygame.draw.line(window, Colorpallette[theme]["grid"], (380, 100), (380, 500), 4)
     pygame.draw.line(window, Colorpallette[theme]["grid"], (100, 220), (500, 220), 4)
     pygame.draw.line(window, Colorpallette[theme]["grid"], (100, 380), (500, 380), 4)
-
-
+    for i, j in enumerate(state):
+        if j == 1:
+            drawcross(Colorpallette[theme]["symbol"], coordinates[i], 40)
+        elif j == -1:
+            pygame.draw.circle(window, Colorpallette[theme]["symbol"], coordinates[i], 44, 4)
+            # Extra 4 due to how thick circles are drawn in pygame
 
 while RUNNING:
     # Code for measuring time
@@ -67,10 +82,9 @@ while RUNNING:
     lastTime = Nowtime
 
     # Game code
-    
-    for e in pygame.event.get(): # All events (Mouse click, keypress, etc. since last call)
+    for e in pygame.event.get():
         gameloop(e)
-    
+
     window.lock()
     window.fill(Colorpallette[theme]["background"])
     draw()
@@ -81,3 +95,10 @@ while RUNNING:
     excess = 1/MAXFPS - dt
     if excess > 0:
         time.sleep(excess)
+
+
+# Additional notes:
+# Boxes are at:
+# (160, 160), (300, 160), (440, 160),
+# (160, 300), (300, 300), (440, 300),
+# (160, 440), (300, 440), (440, 440)
